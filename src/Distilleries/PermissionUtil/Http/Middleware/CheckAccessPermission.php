981507187479fc7aa609914b780cb3880f847827
Maxime Francois <kezho@me.com>
2015-02-24 16:32:23 +1100

    Remove folder test from the test code

--- src/Distilleries/PermissionUtil/Http/Middleware/CheckAccessPermission.php
+++ src/Distilleries/PermissionUtil/Http/Middleware/CheckAccessPermission.php
@@ -7,36 +7,38 @@ use Illuminate\Http\Request;
 class CheckAccessPermission {
 
     protected $app;
-	/**
-	 * The Guard implementation.
-	 *
-	 * @var \Distilleries\PermissionUtil\Helpers\PermissionUtil
-	 */
-	protected $permission;
+    /**
+     * The Guard implementation.
+     *
+     * @var \Distilleries\PermissionUtil\Helpers\PermissionUtil
+     */
+    protected $permission;
 
-	/**
-	 * Create a new filter instance.
-	 *
-	 */
-	public function __construct(Container $app)
-	{
-        $this->app = $app;
-		$this->permission = $app->make('permission-util');
-	}
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  Illuminate\Http\Request\  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-	public function handle(Request $request, Closure $next)
-	{
+    /**
+     * Create a new filter instance.
+     *
+     */
+    public function __construct(Container $app)
+    {
+        $this->app        = $app;
+        $this->permission = $app->make('permission-util');
+    }
 
-		if (!$this->permission->hasAccess($request->route()->getActionName())) {
-			$this->app->abort(403, $this->app->make('translator')->trans('permission-util::errors.unthorized'));
-		}
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
 
-		return $next($request);
-	}
+        if (!$this->permission->hasAccess($request->route()->getActionName()))
+        {
+            $this->app->abort(403, $this->app->make('translator')->trans('permission-util::errors.unthorized'));
+        }
+
+        return $next($request);
+    }
 }
